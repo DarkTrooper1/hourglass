f18e2be6f2fb5ed74a760167644e18d9b846d35a
DarkTrooper1 <146754265+DarkTrooper1@users.noreply.github.com>
2023-11-29 19:45:29 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const gameIframe = () => {
+  document.getElementById('images').style.display = "none"
+  document.getElementById('gameIframe').src = "Drive Mad/"
+  document.getElementById('gamePage').style.display = "block"
+}

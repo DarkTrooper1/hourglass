7486a111b75b72c3b62473ffb17ef3073e773a91
DarkTrooper1 <146754265+DarkTrooper1@users.noreply.github.com>
2023-11-29 23:00:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const gameIframe = () => {
   document.getElementById('gameIframe').src = "Drive Mad/"
   document.getElementById('gamePage').style.display = "block"
 }
+
+const displayBody = () => {
+  const vid = document.getElementById('myVideo')
+  vid.style.display = "none"
+  const content = document.getElementById('content')
+  content.style.display = "block"
+}
